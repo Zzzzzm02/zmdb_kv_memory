3fe652450877dc1f11f43f4ff6bd76a70e52979a
Zzzzzm02 <1056663997@qq.com>
2024-07-29 16:51:00 +0800

    重新调整目录， 然后更改注释

--- fio/file_io.go
+++ fio/file_io.go
@@ -2,10 +2,9 @@ package fio
 
 import "os"
 
-// FileIO 标准系统文件 IO
+// FileIO 封装了os里面的文件描述符
 type FileIO struct {
 	fd *os.File // 系统文件描述符
-
 }
 
 // NewFileIOManager 初始化文件IO方法
@@ -23,18 +22,22 @@ func NewFileIOManager(fileName string) (*FileIO, error) {
 	return &FileIO{fd: fd}, nil
 }
 
+// Read 读文件
 func (fio *FileIO) Read(b []byte, offset int64) (int, error) {
 	return fio.fd.ReadAt(b, offset)
 }
 
+// Write 写入内容
 func (fio *FileIO) Write(b []byte) (int, error) {
 	return fio.fd.Write(b)
 }
 
+// Sync 再文件同步到磁盘
 func (fio *FileIO) Sync() error {
 	return fio.fd.Sync()
 }
 
+// Close 关闭文件
 func (fio *FileIO) Close() error {
 	return fio.fd.Close()
 }
